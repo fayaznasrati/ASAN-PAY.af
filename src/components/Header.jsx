d2fb30841ab12777e157fab850315f14c1d0344a
majdod behnam <Ewallet@afghan-pay.com>
2025-12-10 13:20:33 +0430

    Header respnsive completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 "use client"
+import { Menu, X } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
-
+    
 
     return(
-        <div className=" max-w-screen-2xl mx-auto z-[9999999] fixed inset-0 w-full h-[10px]  px-[100px] mt-4">
-            <div className="align-center bg-gradient-to-b h-[60px] relative transition-all duration-300 from-[#185146]/80 hover:blur-[20] to-[#1d5c37]/80   flex px-8 py-3 rounded-full justify-between items-center  ">
+        <div className=" max-w-screen-2xl mx-auto z-[9999999] fixed inset-0 w-full xl:h-[10px]  xl:px-[100px] xl:mt-4">
+            <div className={`  ${isOpen ? "bg-gradient-to-b md:bg-gradient-to-b  md:from-[#010c0a]/0 md:to-[#f6fa0d]/0 from-[#33B99F] to-[#0F433D] h-[100vh] " : " "}`}>
+            <div className={`align-center  xl:bg-gradient-to-b xl:h-[60px]   relative transition-all duration-300 xl:from-[#185146]/80 hover:blur-[20] xl:to-[#1d5c37]/80   flex px-8 py-3 rounded-full justify-between items-center  `}>
+
             <div className="w-[130px] h-full">
                 <img src="/logo.png" className="w-full h-full object-cover"/>
             </div>
@@ -36,29 +39,34 @@ const Header = () => {
             <button className="px-5 py-2 md:block hidden bg-gradient-to-r rounded-full cursor-pointer text-[#FFFFFF] from-[#2E8C7A] to-[#0C2621]">
                 Log In
             </button>
-          <button onClick={()=> setIsOpen(true)} className="text-[#fff] md:hidden  cursor-pointer">humberger</button>
-            </div>
+          <button onClick={()=> setIsOpen(!isOpen)} className="text-[#fff] p-2 bg-gradient-to-b from-[#2EBEA2] to-[#113A32] rounded-full md:hidden  cursor-pointer">{isOpen ?  <X  />  :  <Menu />}</button>
+      
               
-
-              {isOpen && <div className="absulote top-1 flex justify-between px-2 bg-amber-400">
+            </div>
+              {isOpen && <div className=" fixed top-[150px] flex xl:hidden   justify-center w-full  flex-col items-center content-center text-center  ">
                 
-                   <ul className="flex text-[16px] text-[#FFFFFF] font-medium  gap-4">
+                   <ul className="flex text-[28px] xl:text-[16px] flex-col text-[#FFFFFF] font-medium  gap-4">
+                    <Link href="/">
                 <li>
                     Home
-                </li>
+                </li></Link>
+                <Link href="/Features">
                 <li>
                     Features
-                </li>
+                </li></Link>
+                <Link href="/Aboutus">
                   <li>
                     About Us
-                </li>
+                </li></Link>
+                <Link href="/Contactus">
                 <li>
                     Contact Us
-                </li>
+                </li></Link>
             </ul>
-            <button className="cursor-pointer" onClick={()=> setIsOpen(false)}>close</button>
+            <button className="cursor-pointer rounded-full px-[100px] mt-[20px] text-[20px] text-[#fff] py-4 bg-linear-to-r from-[#27CAAB] to-[#0A2B25]" >Login</button>
                 
                 </div>}
+</div>
         </div>
     )
 }
